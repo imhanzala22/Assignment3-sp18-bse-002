db455dabd99d1e8a210e718bff986888b5709627
imhanzala22 <71677063+imhanzala22@users.noreply.github.com>
2020-12-01 11:47:29 -0800

    without change

--- MyBookShop/Model/Book.js
+++
@@ -1,9 +0,0 @@
-  
-var mongoose = require("mongoose");
-
-var BookSchema = mongoose.Schema({
-  name: String,
-  price: String,
-});
-const Book = mongoose.model("Books", BookSchema);
-module.exports = Book;
--- model/Books.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require("mongoose");
-
-var BooksSchema = mongoose.Schema({
-  name: String,
-  price: String,
-});
-const Books = mongoose.model("Books", BooksSchema);
-module.exports = Books;
