496653c7e1d69c1f2443654e2da1cf33a45e4584
imhanzala22 <71677063+imhanzala22@users.noreply.github.com>
2020-12-01 02:15:39 -0800

    error solution

--- MyBookShop/routes/MyBooks.js
+++ MyBookShop/routes/MyBooks.js
@@ -3,15 +3,23 @@ var router = express.Router();
 var Book = require("../Model/Book");
 
 /* GET home page. */
-//router.get('/', async function(req, res, next) {
-  //let Book= await Book.find();
-//  console.log(Books);
- // res.render('MyBooks/list' , { title : "Books in database", Books } );
-//});
+
 router.get("/",async function(req,res,next){
   let book= await Book.find();
  // console.log(book);
   res.render("MyBook/list",{title:"Books in List",MyBooks:book});
-})
+});
+router.get("/MyBook/add",async function(req,res,next){
+  res.render("MyBook/add");
+ });
+// router.post("/add", async function (req, res, next) {
+//   let book = new Book(req.body);
+//   await book.save();
+//   res.redirect("/MyBook");
+// });
 
-module.exports = router;
+// router.get("/delete/:id", async function (req, res, next) {
+//   let book = await Books.findByIdAndDelete(req.params.id);
+//   res.redirect("/MyBook");
+// });
+// module.exports = router;
